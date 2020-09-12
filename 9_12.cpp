5805eed578433cadde5c8db6a390e6a6575cff95
dragonlogin <1241910692@qq.com>
2020-09-12 16:27:40 +0800

    组合幸运数字

--- 9_12.cpp
+++ 9_12.cpp
@@ -49,26 +49,81 @@
 //}
 
 
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//int main() {
+//    vector<int> arr;
+//    int x;
+//    while (cin >> x) arr.push_back(x);
+//    vector<long long > dp(7,0), t(7,0);
+//
+//    for (auto num : arr) {
+//        for (auto a : dp) {
+//            for (int k = 0; k < 7; ++ k)
+//                if ((num + a) % 7 == k)
+//                    t[k] = max(num + a, t[k]);
+//        }
+//        dp = t;
+//    }
+//    long long res = -1;
+//    if (dp[0])
+//        res = dp[0];
+//    cout << res << endl;
+//    return 0;
+//}
+
 #include <bits/stdc++.h>
 using namespace std;
+struct TreeNode {
+    TreeNode *left, *right;
+    int val;
+    TreeNode(int val) : left(NULL), right(NULL), val(val) {}
+};
+
+int n, m;
+int l, r;
+string s;
+map<int, TreeNode*> mp;
+int res;
+
+int dfs(TreeNode *r) {
+    //cout << r->val << endl;
+    if (!r) return 0;
+
+    int lv = dfs(r->left);
+    int rv = dfs(r->right);
+
+    int val = lv + rv;
+    if (!r->left && !r->right) val += 1;
+    if (val == 2) res ++ ;
+    return val;
+}
+
 
 int main() {
-    vector<int> arr;
-    int x;
-    while (cin >> x) arr.push_back(x);
-    vector<long long > dp(7,0), t(7,0);
-
-    for (auto num : arr) {
-        for (auto a : dp) {
-            for (int k = 0; k < 7; ++ k)
-                if ((num + a) % 7 == k)
-                    t[k] = max(num + a, t[k]);
+    cin >> n >> m;
+    while (m --) {
+        cin >> l >> s >> r;
+        if (mp.find(l) == mp.end()) {
+            auto r = new TreeNode(l);
+            mp[l] = r;
         }
-        dp = t;
+        if (mp.find(r) == mp.end()) {
+            auto rr = new TreeNode(r);
+            mp[r] = rr;
+        }
+        if(s == "left") {
+            mp[l]->left = mp[r];
+
+        } else if (s == "right") {
+            mp[l]->right = mp[r];
+        }
     }
-    long long res = -1;
-    if (dp[0])
-        res = dp[0];
+
+    dfs(mp[1]);
     cout << res << endl;
     return 0;
 }
+
+
